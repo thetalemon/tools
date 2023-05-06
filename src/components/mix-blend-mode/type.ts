78ea7d9ab2342dd19d16aaa2c2e5fd2b83f748d8
thetalemon <sasami0314@gmail.com>
2023-05-06 18:36:25 +0900

    refactor img

---
+++ src/components/mix-blend-mode/type.ts
@@ -0,0 +1,10 @@
+export type ImgOptions = {
+	red: number;
+	green: number;
+	blue: number;
+	alpha: number;
+	blendMode: string;
+	top: number;
+	left: number;
+	layer: number;
+};
--- src/routes/mix-blend-mode/+page.svelte
+++ src/routes/mix-blend-mode/+page.svelte
@@ -2,9 +2,11 @@
 	import InputRgba from '../../components/mix-blend-mode/inputRgba.svelte';
 	import InputSlider from '../../components/mix-blend-mode/inputSlider.svelte';
 	import BasicLayout from '../../components/layout/basicLayout.svelte';
+	import MixBlendModeImg from '../../components/mix-blend-mode/mixBlendModeImg.svelte';
+	import type { ImgOptions } from '../../components/mix-blend-mode/type';
 
-	export let imgSrc: string = 'Ak81Vc-kCf4';
-	let text = {
+	let imgSrc: string = 'Ak81Vc-kCf4';
+	let text: ImgOptions = {
 		red: 150,
 		green: 200,
 		blue: 255,
@@ -14,7 +16,7 @@
 		left: 80,
 		layer: 0
 	};
-	let mask = {
+	let mask: ImgOptions = {
 		red: 10,
 		green: 60,
 		blue: 100,
@@ -73,24 +75,7 @@
 				</section>
 			</div>
 		</section>
-
-		<div
-			class="contentWrapper"
-			style="
-    --text-blend-mode: {text.blendMode}; 
-    --text-top: {text.top}px; --text-left: {text.left}px; 
-    --text-red: {text.red}; --text-green: {text.green}; --text-blue: {text.blue}; --text-alpha: {text.alpha}; 
-    --text-z-index: {text.layer};
-    --mask-blend-mode: {mask.blendMode}; 
-    --mask-top: {mask.top}px; --mask-left: {mask.left}px; 
-    --mask-red: {mask.red}; --mask-green: {mask.green}; --mask-blue: {mask.blue}; --mask-alpha: {mask.alpha};
-    --mask-z-index: {mask.layer};"
-		>
-			<div class="imgWrapper">
-				<p class="text">Welcome</p>
-				<img src={`https://source.unsplash.com/${imgSrc ? imgSrc : 'Ak81Vc-kCf4'}`} alt="画像" />
-			</div>
-		</div>
+		<MixBlendModeImg {text} {mask} {imgSrc} />
 	</div>
 </BasicLayout>
 
@@ -107,43 +92,5 @@
 
 	.settings {
 		display: flex;
-	}
-	.contentWrapper {
-		display: flex;
-		justify-content: center;
-		margin: 48px;
-		.imgWrapper {
-			position: relative;
-			margin: 24px;
-			img {
-				width: 500px;
-			}
-			.text {
-				display: block;
-				position: absolute;
-				font-size: 88px;
-				font-weight: 700;
-				top: var(--text-top);
-				left: var(--text-left);
-				margin: 0;
-				z-index: var(--text-z-index);
-				line-height: 1;
-				color: rgba(var(--text-red), var(--text-green), var(--text-blue), var(--text-alpha));
-				mix-blend-mode: var(--text-blend-mode);
-			}
-
-			&::before {
-				position: absolute;
-				display: block;
-				width: calc(100% + 48px);
-				height: calc(100% + 48px);
-				content: '';
-				top: var(--mask-top);
-				left: var(--mask-left);
-				background: rgba(var(--mask-red), var(--mask-green), var(--mask-blue), var(--mask-alpha));
-				z-index: var(--mask-z-index);
-				mix-blend-mode: var(--mask-blend-mode);
-			}
-		}
 	}
 </style>
